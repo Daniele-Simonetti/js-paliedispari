4ad886acf79523910c07d27aebc8ac1d2d6453a5
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-29 17:13:27 +0100

    testing

--- pariedispari/script.js
+++ pariedispari/script.js
@@ -7,39 +7,42 @@
 // (corrispondenza tra scelta pari e dispari e somma pari o dispari)
 // In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
 
-
+// creo una funzione per il check del pari e dispari
+function controllo(risultatoSomma) {
+  // aggiungo una variabile boleana per il confronto
+  let find = false;
+  // creo la condizione di pari o dispari
+  if (sommaNumeri % 2 == 0) {
+    find = true;
+    console.log('la somma è pari');
+  } else {
+    console.log('la somma è dispari');
+  }
+  // se ho scelto pari e la somma è pari e viceversa allora ho vinto
+  if (find == true && pariOdispari == 'pari') {
+    console.log('hai vinto');
+  } else if (find == false && pariOdispari == 'dispari') {
+    console.log('hai vinto');
+  } else {
+    console.log('hai perso');
+  }
+  return controllo
+}
 // chiedo all'utente di scegliere pari o dispari
 let pariOdispari = prompt('scegli pari o dispari');
-console.log('prima condizione di vittoria ', pariOdispari);
+console.log('prima condizione di vittoria: ', pariOdispari);
 
 // chiedo all'utente un numero da 1 a 5
 const numeroUtente = parseInt(prompt('inserisci un numero da 1 a 5'));
-console.log('numero scelto da utente ', numeroUtente);
+console.log('numero scelto da utente: ', numeroUtente);
 
 // genero un numero random da 1 a 5 per il computer
 const numeroComputer = parseInt(Math.floor(Math.random() * 5) + 1);
-console.log('numero random del computer ', numeroComputer);
+console.log('numero random del computer: ', numeroComputer);
 
 // sommiamo i due numeri
 let sommaNumeri = numeroComputer + numeroUtente;
-console.log('somma numero utente e computer ', sommaNumeri);
+console.log('somma numero utente e computer: ', sommaNumeri);
 
-// aggiungo una variabile boleana per il confronto
-let find = false;
-
-// creo la condizione di pari o dispari
-if (sommaNumeri % 2 == 0) {
-  find = true;
-  console.log('la somma è pari');
-} else {
-  console.log('la somma è dispari');
-}
-
-// se ho scelto pari e la somma è pari e viceversa allora ho vinto
-if (find == true && pariOdispari == 'pari') {
-  alert('HAI VINTO');
-} else if (find == false && pariOdispari == 'dispari') {
-  alert('HAI VINTO');
-} else {
-  alert('HAI PERSO');
-}
+// collego la mia funzione
+let risultatoSomma = controllo;
